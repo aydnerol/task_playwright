f425657de98c3f3e6de27c2f9e8d8d559b4c589b
aydnerol <aydinerol4106@gmail.comcls>
2022-08-26 02:39:13 +0200

    Fill the form part2

--- tests/submit.task.spec.js
+++ tests/submit.task.spec.js
@@ -42,51 +42,49 @@ test("should go to ToolsQA main page", async ({ page }) => {
   await page.locator(".react-datepicker__year-select").click();
   await page.locator(".react-datepicker__year-select").selectOption("1989");
 
-  // // Select 5
-  // await page.locator("select").first().selectOption("5");
-  // // Select 4
-  // await page.locator("select").first().selectOption("4");
+  // Select 4
+  await page.locator("select").first().selectOption("4");
   // Click [aria-label="Choose Monday\, May 15th\, 1989"]
-  // await page
-  //   .locator('[aria-label="Choose Monday\\, May 15th\\, 1989"]')
-  //   .click();
+  await page
+    .locator('[aria-label="Choose Monday\\, May 15th\\, 1989"]')
+    .click();
 
-  // await page
-  //   .locator('[aria-label="Choose Monday\\, May 15th\\, 1989"]')
-  //   .click();
-  // // Click .subjects-auto-complete__value-container
-  // await page.locator(".subjects-auto-complete__value-container").click();
-  // // Fill text=Subjects 0 results available. Use Up and Down to choose options, press Enter to  >> input[type="text"]
-  // await page
-  //   .locator(
-  //     'text=Subjects 0 results available. Use Up and Down to choose options, press Enter to  >> input[type="text"]'
-  //   )
-  //   .fill("task");
-  // // Click text=Music
-  // await page.locator("text=Music").click();
-  // // Click input[type="file"]
-  // await page.locator('input[type="file"]').click();
-  // // Upload erol.jpeg
-  // await page.locator('input[type="file"]').setInputFiles("../asset/erol.jpeg");
-  // // Click textarea
-  // await page.locator("textarea").click();
-  // // Fill textarea
-  // await page.locator("textarea").fill("Mol");
+  // Click .subjects-auto-complete__value-container
+  await page.locator(".subjects-auto-complete__value-container").click();
+  // Fill text=Subjects 0 results available. Use Up and Down to choose options, press Enter to  >> input[type="text"]
+  await page
+    .locator(
+      'text=Subjects 0 results available. Use Up and Down to choose options, press Enter to  >> input[type="text"]'
+    )
+    .fill("task");
+  // Click text=Music
+  await page.locator("text=Music").click();
 
-  // // Click #state svg
-  // await page.locator("#state svg").click();
-  // // Click #state div:has-text("Select State") >> nth=1
-  // await page.locator('#state div:has-text("Select State")').nth(1).click();
-  // // Click #react-select-3-option-0
-  // await page.locator("#react-select-3-option-0").click();
-  // // Click #city div:has-text("Select City") >> nth=1
-  // await page.locator('#city div:has-text("Select City")').nth(1).click();
-  // // Click #react-select-4-option-0
-  // await page.locator("#react-select-4-option-0").click();
-  // // Click text=Submit
-  // await page.locator("text=Submit").click();
-  // // Click text=Close
-  // await page.locator("text=Close").click();
+  // Click input[type="file"]
+  await page.locator('input[type="file"]').click();
+  // Upload erol.jpeg
+  // await page.locator('input[type="file"]').setInputFiles("erol.jpeg");
 
-  await page.pause();
+  await page.locator('input[type="file"]').setInputFiles("./assets/erol.jpeg");
+
+  // Click textarea
+  await page.locator("textarea").click();
+  // Fill textarea
+  await page.locator("textarea").fill("Mol");
+
+  // Click #state div:has-text("Select State") >> nth=1
+  await page.locator('#state div:has-text("Select State")').nth(1).click();
+  // Click #react-select-3-option-0
+  await page.locator("#react-select-3-option-0").click();
+  // Click #city div:has-text("Select City") >> nth=1
+  await page.locator('#city div:has-text("Select City")').nth(1).click();
+  // Click #react-select-4-option-0
+  await page.locator("#react-select-4-option-0").click();
+
+  // Click text=Submit
+  await page.locator("text=Submit").click();
+  // Click text=Close
+  await page.locator("text=Close").click();
+
+  // await page.pause();
 });
